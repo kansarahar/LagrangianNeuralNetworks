fixed hessian calculation

lnn.py
@@ -7,20 +7,6 @@ from torch.autograd.functional import jacobian, hessian
 #   Model
 # ----------------------------------------------------------
 
-class Lagrangian(nn.Module):
-    def __init__(self, num_params, hidden_dimensions = 128):
-        super().__init__()
-        self.output = nn.Sequential(
-            nn.Linear(2*num_params, hidden_dimensions),
-            nn.Softplus(),
-            nn.Linear(hidden_dimensions, hidden_dimensions),
-            nn.Softplus(),
-            nn.Linear(hidden_dimensions, 1)
-        )
-    def forward(self, x):
-        # x = (q0, q1, ..., q0_t, q1_t, ...)
-        return self.output(x)
-
 class LNN(nn.Module):
     '''
     A neural network composed of a lagrangian encoder and a derivative layer.
@@ -34,12 +20,20 @@ class LNN(nn.Module):
     def __init__(self, num_params, hidden_dimensions = 128):
         super().__init__()
         self.num_params = num_params
-        self.lagrangian = Lagrangian(num_params, hidden_dimensions)
+
+        self.lagrangian = nn.Sequential(
+            nn.Linear(2*num_params, hidden_dimensions),
+            nn.Softplus(),
+            nn.Linear(hidden_dimensions, hidden_dimensions),
+            nn.Softplus(),
+            nn.Linear(hidden_dimensions, 1)
+        )
 
     def forward(self, x):
+        # x = (q0, q1, ..., q0_t, q1_t, ...)
         H = hessian(self.lagrangian, x, True, True)
         D = jacobian(self.lagrangian, x, True, True)
         dq0 = D[0, :self.num_params]
         dq1 = H[self.num_params:, self.num_params:]
-        dq2 = H[:self.num_params, self.num_params:]
+        dq2 = H[self.num_params:, :self.num_params]
         return torch.inverse(dq1) @ (dq0 - dq2 @ x[self.num_params:])