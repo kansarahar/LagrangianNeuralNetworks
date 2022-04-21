4c229d680875f3a20583898a83a00586a0d75dd8
Haresh Kansara <kansarahar@gmail.com>
2022-04-21 01:49:15 -0500

    added physics for spring pendulum and cart pendulum

--- physics.py
+++ physics.py
@@ -36,6 +36,19 @@ def RK4_step(func, y0, t0, dt=0.01):
 
 class Double_Pendulum:
     def __init__(self, t1, t2, w1=0, w2=0, m1=1, m2=1, l1=1, l2=1, g=9.8, lnn=None):
+        '''
+        :param t1: the initial angle of the first mass w.r.t the y-axis (radians)
+        :param t2: the initial angle of the second mass w.r.t the y-axis (radians)
+        :param w1: the initial time rate of change of t1 (radians/s)
+        :param w2: the initial time rate of change of t2 (radians/s)
+        :param m1: the mass of the first pendulum (kg)
+        :param m1: the mass of the second pendulum (kg)
+        :param l1: the length of the first pendulum wire (m)
+        :param l2: the length of the second pendulum wire (m)
+        :param g: the acceleration due to gravity (m/s^2)
+        :param lnn: an optional lnn model that can be attached to this experiment
+        '''
+        
         self.m1 = m1
         self.m2 = m2
         self.l1 = l1
@@ -133,4 +146,209 @@ class Double_Pendulum:
         self.state += step
         self.state[0] %= (2*np.pi) # angles should be [0, 2pi)
         self.state[1] %= (2*np.pi)
+        return step
+
+class Spring_Pendulum:
+    def __init__(self, theta, r, theta_t=0, r_t=0, m=1, k=10, r0=1, g=9.8, lnn=None):
+        '''
+        :param theta: the initial angle of the mass w.r.t the y-axis (radians)
+        :param r: the initial length of the spring (m)
+        :param theta_t: the initial time rate of change of theta (radians/s)
+        :param r_t: the initial time rate of change of r (m/s)
+        :param m: the mass of the pendulum (kg)
+        :param k: the spring constant (N/m)
+        :param r0: the equilibrium length of the spring (m)
+        :param g: the acceleration due to gravity (m/s^2)
+        :param lnn: an optional lnn model that can be attached to this experiment
+        '''
+        
+        self.m = m
+        self.r0 = r0
+        self.k = k
+        self.g = g
+        self.state = tensor([theta, r, theta_t, r_t]).float()
+        self.lnn = lnn
+
+    def get_cartesian_coords(self, state=None):
+        state = state if state is not None else self.state
+        theta, r, theta_t, r_t = state
+        x, y = r * torch.sin(theta), r * torch.cos(theta)
+        return torch.stack([x, y])
+
+    def get_potential_energy(self, state=None):
+        state = state if state is not None else self.state
+        theta, r, theta_t, r_t = state
+        return -self.m * self.g * r * torch.cos(theta) + 0.5 * self.k * (r - self.r0)**2
+
+    def get_kinetic_energy(self, state=None):
+        state = state if state is not None else self.state
+        theta, r, theta_t, r_t = state
+        return 0.5 * self.m * (r_t**2 + (r * theta_t)**2)
+
+    def get_total_energy(self, state=None):
+        state = state if state is not None else self.state
+        return self.get_kinetic_energy(state) + self.get_potential_energy(state)
+
+    def get_lagrangian(self, state=None):
+        state = state if state is not None else self.state
+        return self.get_kinetic_energy(state) - self.get_potential_energy(state)
+
+    def get_derivs_analytical(self, state=None, t=0):
+        '''
+        :param state: the angles and angular velocities of the two masses
+        :param t: dummy variable for runge-kutta calculation
+        :returns: the angular velocities and accelerations of the two masses
+        '''
+        state = state if state is not None else self.state
+
+        m, k, r0, g = self.m, self.k, self.r0, self.g
+        theta, r, theta_t, r_t = state.tolist() # angles and angular velocities of the two masses
+
+        r = r if r is not 0 else 1e-3
+        theta_tt = -(g * np.sin(theta) + 2 * r_t * theta_t) / r
+        r_tt = r * theta_t**2 + g * np.cos(theta) - k * (r - r0) / m
+
+        # return derivative of state
+        return tensor([theta_t, r_t, theta_tt, r_tt]).float()
+
+    def get_derivs_lagrangian(self, state=None, t=0):
+        state = state if state is not None else self.state
+ 
+        H = hessian(self.get_lagrangian, state)
+        J = jacobian(self.get_lagrangian, state)
+
+        A = J[:2]
+        B = H[2:, 2:]
+        C = H[2:, :2]
+
+        q_tt = torch.linalg.pinv(B) @ (A - C @ state[2:])
+        return torch.cat((state[2:], q_tt))
+
+    def get_derivs_lnn(self, state=None, t=0):
+        state = state if state is not None else self.state
+        if (not self.lnn):
+            print('No LNN is attached to this experiment')
+            sys.exit(1)
+        lnn_result = self.lnn(state)
+        return torch.cat((state[2:], lnn_result))
+
+    def step_analytical(self, dt=0.01):
+        step = RK4_step(self.get_derivs_analytical, self.state, 0, dt)
+        self.state += step
+        self.state[0] %= (2*np.pi) # angles should be [0, 2pi)
+        return step
+
+    def step_lagrangian(self, dt=0.01):
+        step = RK4_step(self.get_derivs_lagrangian, self.state, 0, dt)
+        self.state += step
+        self.state[0] %= (2*np.pi) # angles should be [0, 2pi)
+        return step
+
+    def step_lnn(self, dt=0.01):
+        step = RK4_step(self.get_derivs_lnn, self.state, 0, dt)
+        self.state += step
+        self.state[0] %= (2*np.pi) # angles should be [0, 2pi)
+        return step
+
+class Cart_Pendulum:
+    def __init__(self, d, theta, d_t=0, theta_t=0, mc=1, mp=1, k=10, d0=1, l=1, g=9.8, lnn=None):
+        '''
+        :param d: the initial length of the spring (m)
+        :param theta: the initial angle of the mass w.r.t the y-axis (radians)
+        :param d_t: the initial time rate of change of d (m/s)
+        :param theta_t: the initial time rate of change of theta (radians/s)
+        :param mc: the mass of the cart (kg)
+        :param mp: the mass of the pendulum (kg)
+        :param k: the spring constant (m)
+        :param d0: the equilibrium length of the spring (m)
+        :param l: the length of the pendulum wire (m)
+        :param g: the acceleration due to gravity (m/s^2)
+        :param lnn: an optional lnn model that can be attached to this experiment
+        '''
+
+        self.mc = mc
+        self.mp = mp
+        self.d0 = d0
+        self.k = k
+        self.l = l
+        self.g = g
+        self.state = tensor([d, theta, d_t, theta_t]).float()
+        self.lnn = lnn
+
+    def get_cartesian_coords(self, state=None):
+        state = state if state is not None else self.state
+        d, theta, d_t, theta_t = state
+        xc, yc, xp, yp = d - self.d0, tensor(0), d - self.d0 + self.l * torch.sin(theta), self.l * torch.cos(theta)
+        return torch.stack([xc, yc, xp, yp])
+
+    def get_potential_energy(self, state=None):
+        state = state if state is not None else self.state
+        d, theta, d_t, theta_t = state
+        return -self.mp * self.g * torch.cos(theta) + 0.5 * self.k * (d - self.d0)**2
+
+    def get_kinetic_energy(self, state=None):
+        state = state if state is not None else self.state
+        d, theta, d_t, theta_t = state
+        return 0.5 * (self.mc * d_t**2 + self.mp * (self.l * theta_t)**2)
+
+    def get_total_energy(self, state=None):
+        state = state if state is not None else self.state
+        return self.get_kinetic_energy(state) + self.get_potential_energy(state)
+
+    def get_lagrangian(self, state=None):
+        state = state if state is not None else self.state
+        return self.get_kinetic_energy(state) - self.get_potential_energy(state)
+
+    def get_derivs_analytical(self, state=None, t=0):
+        '''
+        :param state: the angles and angular velocities of the two masses
+        :param t: dummy variable for runge-kutta calculation
+        :returns: the angular velocities and accelerations of the two masses
+        '''
+        state = state if state is not None else self.state
+
+        d, theta, d_t, theta_t = state.tolist()
+
+        d_tt = -self.k * (d - self.d0) / self.mc
+        theta_tt = -self.g * np.sin(theta) / self.l**2
+        # return derivative of state
+        return tensor([d_t, theta_t, d_tt, theta_tt]).float()
+
+    def get_derivs_lagrangian(self, state=None, t=0):
+        state = state if state is not None else self.state
+ 
+        H = hessian(self.get_lagrangian, state)
+        J = jacobian(self.get_lagrangian, state)
+
+        A = J[:2]
+        B = H[2:, 2:]
+        C = H[2:, :2]
+
+        q_tt = torch.linalg.pinv(B) @ (A - C @ state[2:])
+        return torch.cat((state[2:], q_tt))
+
+    def get_derivs_lnn(self, state=None, t=0):
+        state = state if state is not None else self.state
+        if (not self.lnn):
+            print('No LNN is attached to this experiment')
+            sys.exit(1)
+        lnn_result = self.lnn(state)
+        return torch.cat((state[2:], lnn_result))
+
+    def step_analytical(self, dt=0.01):
+        step = RK4_step(self.get_derivs_analytical, self.state, 0, dt)
+        self.state += step
+        self.state[0] %= (2*np.pi) # angles should be [0, 2pi)
+        return step
+
+    def step_lagrangian(self, dt=0.01):
+        step = RK4_step(self.get_derivs_lagrangian, self.state, 0, dt)
+        self.state += step
+        self.state[0] %= (2*np.pi) # angles should be [0, 2pi)
+        return step
+
+    def step_lnn(self, dt=0.01):
+        step = RK4_step(self.get_derivs_lnn, self.state, 0, dt)
+        self.state += step
+        self.state[0] %= (2*np.pi) # angles should be [0, 2pi)
         return step
--- simulator.py
+++ simulator.py
@@ -6,7 +6,7 @@ import pygame
 import numpy as np
 import torch
 
-import physics
+from physics import Double_Pendulum, Spring_Pendulum, Cart_Pendulum
 from lnn import LNN
 
 # ----------------------------------------------------------
@@ -18,7 +18,7 @@ parser = argparse.ArgumentParser(
     formatter_class=argparse.ArgumentDefaultsHelpFormatter
 )
 
-parser.add_argument('--experiment', dest='experiment', type=str, choices=['double_pendulum'], default='double_pendulum', help='the type of experiment that the LNN is learning')
+parser.add_argument('--experiment', dest='experiment', type=str, choices=['double_pendulum', 'spring_pendulum', 'cart_pendulum'], default='double_pendulum', help='the type of experiment that the LNN is learning')
 parser.add_argument('--model-name', dest='model_name', type=str, default='model.pt', help='the name of the LNN model to be saved or loaded')
 parser.add_argument('--lnn', dest='lnn', action='store_true', help='use the trained model instead of the analytical solution')
 
@@ -29,7 +29,7 @@ args = parser.parse_args()
 # ----------------------------------------------------------
 
 dir_name = os.path.dirname(os.path.abspath(__file__))
-model_dir = os.path.join(dir_name, 'models', 'double_pendulum_models')
+model_dir = os.path.join(dir_name, 'models', '%s_models' % args.experiment)
 model_path = os.path.join(model_dir, args.model_name)
 
 # ---------------------------------------------------------- 
@@ -58,10 +58,17 @@ default_font = pygame.font.Font(pygame.font.get_default_font(), 16)
 #   Main Event Loop
 # ----------------------------------------------------------
 
+double_pendulum = Double_Pendulum(np.pi*0.26, np.pi*0.82)
+spring_pendulum = Spring_Pendulum(np.pi*0.26, 1.2)
+cart_pendulum = Cart_Pendulum(2, np.pi*0.26)
+
 model = LNN(2)
-model.load_state_dict(torch.load(model_path))
-model.eval()
-double_pendulum = physics.Double_Pendulum(np.pi*0.26, np.pi*0.82, 0, 0, 1, 1, 1, 1, 9.8, model)
+if args.lnn:
+    model.load_state_dict(torch.load(model_path))
+    model.eval()
+    double_pendulum.lnn = model
+    spring_pendulum.lnn = model
+    cart_pendulum.lnn = model
 
 while True:
     for event in pygame.event.get():
@@ -78,27 +85,86 @@ while True:
         #        print(double_pendulum.state)
         #    if (event.key == pygame.K_a or event.key == pygame.K_LEFT):
         #        double_pendulum.step_analytical(-0.01)
+
+    mid_width, mid_height = bg_surface.get_width()//2, bg_surface.get_height()//2
+    if (args.experiment == 'double_pendulum'):
    
-    x1, y1, x2, y2 = 0.1 * px * double_pendulum.get_cartesian_coords().detach().numpy()
-    double_pendulum.step_lnn() if args.lnn else double_pendulum.step_lagrangian()
+        x1, y1, x2, y2 = 0.1 * px * double_pendulum.get_cartesian_coords().detach().numpy()
+        double_pendulum.step_lnn() if args.lnn else double_pendulum.step_lagrangian()
+        
+        vertical_offset = 100
+        mass1_pos = (bg_surface.get_width()//2 + x1, y1 + vertical_offset)
+        mass2_pos = (bg_surface.get_width()//2 + x2, y2 + vertical_offset)
+
+        screen.blit(bg_surface, ((px - bg_surface.get_width())//2, (px - bg_surface.get_height())//2)) # block image transfer (one surface imposed on another surface)
+        bg_surface.fill('White')
+        pygame.draw.line(bg_surface, 'Black', (mid_width, vertical_offset), mass1_pos)
+        pygame.draw.line(bg_surface, 'Black', mass1_pos, mass2_pos)
+        pygame.draw.circle(bg_surface, 'Black', (mid_width, vertical_offset), 4)
+        pygame.draw.circle(bg_surface, 'Red', mass1_pos, 10)
+        pygame.draw.circle(bg_surface, 'Blue', mass2_pos, 10)
+
+        potential_energy_text = default_font.render('Potential Energy: %s J' % round(double_pendulum.get_potential_energy().item(), 3), True, (0, 0, 0))
+        kinetic_energy_text = default_font.render('Kinetic Energy: %s J' % round(double_pendulum.get_kinetic_energy().item(), 3), True, (0, 0, 0))
+        total_energy_text = default_font.render('Total Energy: %s J' % round(double_pendulum.get_total_energy().item(), 3), True, (0, 0, 0))
+        bg_surface.blit(potential_energy_text, (10, (mid_height + 100)))
+        bg_surface.blit(kinetic_energy_text, (10, (mid_height + 125)))
+        bg_surface.blit(total_energy_text, (10, (mid_height + 150)))
+
+        pygame.display.update()
+        clock.tick(120) # prevents the while loop from running faster than 60Hz
     
-    vertical_offset = 20
-    mass1_pos = (bg_surface.get_width()//2 + x1, y1 + vertical_offset)
-    mass2_pos = (bg_surface.get_width()//2 + x2, y2 + vertical_offset)
+    if (args.experiment == 'spring_pendulum'):
+        spring_pendulum.lnn = model
+   
+        x, y = 0.1 * px * spring_pendulum.get_cartesian_coords().detach().numpy()
+        spring_pendulum.step_lnn() if args.lnn else spring_pendulum.step_lagrangian()
+        
+        vertical_offset = 100
+        mass_pos = (bg_surface.get_width()//2 + x, y + vertical_offset)
 
-    screen.blit(bg_surface, ((px - bg_surface.get_width())//2, (px - bg_surface.get_height())//2)) # block image transfer (one surface imposed on another surface)
-    bg_surface.fill('White')
-    pygame.draw.line(bg_surface, 'Black', (bg_surface.get_width()//2, vertical_offset), mass1_pos)
-    pygame.draw.line(bg_surface, 'Black', mass1_pos, mass2_pos)
-    pygame.draw.circle(bg_surface, 'Red', mass1_pos, 10)
-    pygame.draw.circle(bg_surface, 'Blue', mass2_pos, 10)
+        screen.blit(bg_surface, ((px - bg_surface.get_width())//2, (px - bg_surface.get_height())//2)) # block image transfer (one surface imposed on another surface)
+        bg_surface.fill('White')
+        pygame.draw.line(bg_surface, 'Black', (mid_width, vertical_offset), mass_pos)
+        pygame.draw.circle(bg_surface, 'Black', (mid_width, vertical_offset), 4)
+        pygame.draw.circle(bg_surface, 'Red', mass_pos, 10)
 
-    potential_energy_text = default_font.render('Potential Energy: %s J' % round(double_pendulum.get_potential_energy().item(), 3), True, (0, 0, 0))
-    kinetic_energy_text = default_font.render('Kinetic Energy: %s J' % round(double_pendulum.get_kinetic_energy().item(), 3), True, (0, 0, 0))
-    total_energy_text = default_font.render('Total Energy: %s J' % round(double_pendulum.get_total_energy().item(), 3), True, (0, 0, 0))
-    bg_surface.blit(potential_energy_text, (10, (bg_surface.get_height()//2 + 100)))
-    bg_surface.blit(kinetic_energy_text, (10, (bg_surface.get_height()//2 + 125)))
-    bg_surface.blit(total_energy_text, (10, (bg_surface.get_height()//2 + 150)))
+        potential_energy_text = default_font.render('Potential Energy: %s J' % round(spring_pendulum.get_potential_energy().item(), 3), True, (0, 0, 0))
+        kinetic_energy_text = default_font.render('Kinetic Energy: %s J' % round(spring_pendulum.get_kinetic_energy().item(), 3), True, (0, 0, 0))
+        total_energy_text = default_font.render('Total Energy: %s J' % round(spring_pendulum.get_total_energy().item(), 3), True, (0, 0, 0))
+        bg_surface.blit(potential_energy_text, (10, (mid_height + 100)))
+        bg_surface.blit(kinetic_energy_text, (10, (mid_height + 125)))
+        bg_surface.blit(total_energy_text, (10, (mid_height + 150)))
 
-    pygame.display.update()
-    clock.tick(120) # prevents the while loop from running faster than 60Hz
+        pygame.display.update()
+        clock.tick(120) # prevents the while loop from running faster than 60Hz
+
+    if (args.experiment == 'cart_pendulum'):
+        spring_pendulum.lnn = model
+   
+        xc, yc, xp, yp = 0.1 * px * cart_pendulum.get_cartesian_coords().detach().numpy()
+        cart_pendulum.step_lnn() if args.lnn else cart_pendulum.step_lagrangian()
+        
+        vertical_offset = 100
+        cart_pos = (bg_surface.get_width()//2 + xc, vertical_offset - 10)
+        mass_pos = (bg_surface.get_width()//2 + xp, yp + vertical_offset)
+
+        screen.blit(bg_surface, ((px - bg_surface.get_width())//2, (px - bg_surface.get_height())//2)) # block image transfer (one surface imposed on another surface)
+        bg_surface.fill('White')
+        cart = pygame.Rect(cart_pos[0]-15, cart_pos[1]-10, 30, 20)
+        pygame.draw.line(bg_surface, 'Black', cart_pos, mass_pos)
+        pygame.draw.line(bg_surface, 'Black', (mid_width - 200, vertical_offset - 10), (cart_pos[0], vertical_offset-10), 1)
+        pygame.draw.line(bg_surface, 'Black', (mid_width - 100, vertical_offset), (mid_width + 100, vertical_offset), 2)
+        pygame.draw.circle(bg_surface, 'Black', (mid_width - 200, vertical_offset - 10), 4)
+        pygame.draw.circle(bg_surface, 'Red', mass_pos, 10)
+        pygame.draw.rect(bg_surface, 'Blue', cart)
+
+        potential_energy_text = default_font.render('Potential Energy: %s J' % round(cart_pendulum.get_potential_energy().item(), 3), True, (0, 0, 0))
+        kinetic_energy_text = default_font.render('Kinetic Energy: %s J' % round(cart_pendulum.get_kinetic_energy().item(), 3), True, (0, 0, 0))
+        total_energy_text = default_font.render('Total Energy: %s J' % round(cart_pendulum.get_total_energy().item(), 3), True, (0, 0, 0))
+        bg_surface.blit(potential_energy_text, (10, (bg_surface.get_height()//2 + 100)))
+        bg_surface.blit(kinetic_energy_text, (10, (bg_surface.get_height()//2 + 125)))
+        bg_surface.blit(total_energy_text, (10, (bg_surface.get_height()//2 + 150)))
+
+        pygame.display.update()
+        clock.tick(120) # prevents the while loop from running faster than 60Hz
